9932b28c8ae2ea6ca12fa23883e11b02159cbbd2
Martin Mair <mair.martin@gmail.com>
2021-10-07 13:24:41 +0200

    added comment for log method

--- examples/01_repository/src/main/java/at/campus02/dbp2/repository/Application.java
+++ examples/01_repository/src/main/java/at/campus02/dbp2/repository/Application.java
@@ -2,6 +2,8 @@ package at.campus02.dbp2.repository;
 
 public class Application {
 
+    // Hilfsmethode, damit der Application Log Output besser erkannt werden kann
+    // (gradle schreibt noch mehr in die Konsole).
     public static void log(String msg) {
         System.out.println("Application:  --> " + msg);
     }
